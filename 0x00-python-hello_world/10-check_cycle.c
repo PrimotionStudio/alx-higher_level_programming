429eb32a513784dce6be53e3e98a9001d42ceafe
Martins <oyedelenewton@gmail.com>
2023-11-27 04:29:11 -0800

    10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,31 +9,18 @@
   */
 int check_cycle(listint_t *list)
 {
-	listint_t *fast = NULL, *slow = NULL;
+	listint_t *fast = list, *slow = list;
 
-	fast = (listint_t *)malloc(sizeof(list));
-	if (!fast)
-		return (-1);
-	slow = (listint_t *)malloc(sizeof(list));
-	if (!slow)
-	{
-		free(fast);
-		return (-1);
-	}
-	fast = list->next;
-	slow = list;
+	if (slow == NULL && fast->next == NULL)
+		return (0);
 	while (fast != NULL && fast->next != NULL && slow != NULL)
 	{
 		if (fast == slow)
 		{
-			free(fast);
-			free(slow);
 			return (1);
 		}
 		fast = fast->next->next;
 		slow = slow->next;
 	}
-	free(fast);
-	free(slow);
 	return (0);
 }
