705ce14d8833c179dc2c4d1ff09711db579deade
Martins <oyedelenewton@gmail.com>
2023-11-28 03:56:06 -0800

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(0, 90):
     last_digit = int(i % 10)
     first_digit = int(i / 10)
     if (int(i / 10) < int(i % 10)):
-        print(f"{int(i / 10)}{int(i % 10)}", end="")
+        print("{}{}".format(int(i / 10), int(i % 10)), end="")
         if (i == 89):
             print("")
         else:
