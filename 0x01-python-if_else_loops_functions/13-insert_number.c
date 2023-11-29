418f452fd54d8e0f95cc9369107b8f3cdea37a99
Martins <oyedelenewton@gmail.com>
2023-11-29 02:31:56 -0800

    13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -5,59 +6,45 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node = NULL, *rep_node = NULL, *prev_node = NULL;
+    listint_t *new_node = NULL, *rep_node = *head, *prev_node = NULL;
 
-	/** If empty list  */
-	if (*head == NULL)
-	{
-		*head = (listint_t *)malloc(sizeof(listint_t));
-		if (*head == NULL)
-			return (NULL);
-		(*head)->n = number;
-		(*head)->next = NULL;
-		return (*head);
-	}
+    /** If empty list  */
+    if (*head == NULL)
+    {
+        *head = (listint_t *)malloc(sizeof(listint_t));
+        if (*head == NULL)
+            return (NULL);
+        (*head)->n = number;
+        (*head)->next = NULL;
+        return (*head);
+    }
 
-	/** Memory Allocation */
-	new_node = (listint_t *)malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
+    /** Memory Allocation */
+    new_node = (listint_t *)malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node->n = number;
-	/** Insert at beginning */
-	if ((*head)->n > number)
-	{
-		new_node->next = *head;
-		(*head)->next = new_node;
-		return (new_node);
-	}
+    new_node->n = number;
+    /** Insert at beginning */
+    if ((*head)->n > number)
+    {
+        new_node->next = *head;
+        (*head)->next = new_node;
+        return (new_node);
+    }
 
-	/** Memory Allocation */
-	rep_node = (listint_t *)malloc(sizeof(listint_t));
-	if (rep_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	prev_node = (listint_t *)malloc(sizeof(listint_t));
-	if (prev_node == NULL)
-	{
-		free(new_node);
-		free(rep_node);
-		return (NULL);
-	}
+    /** Insert at the middle */
+    while (rep_node != NULL)
+    {
+        if (rep_node->n > number)
+            break;
+        /** rep_node === cur_node*/
+        prev_node = rep_node;
+        rep_node = rep_node->next;
+    }
 
-	/** Insert at the middle */
-	while (rep_node != NULL )
-	{
-		if (rep_node->n > number)
-			break;
-		/** rep_node === cur_node*/
-		prev_node = rep_node;
-		rep_node = rep_node->next;
-	}
-
-	new_node->next = rep_node;
-	prev_node->next = new_node;
-	return (new_node);
+    new_node->next = rep_node;
+    prev_node->next = new_node;
+    return (new_node);
 }
+
