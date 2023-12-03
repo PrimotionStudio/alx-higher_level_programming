f860397b0f5579e07eefb57190ff379012b2ae74
Martins <oyedelenewton@gmail.com>
2023-12-03 05:10:32 -0800

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if (len(my_list) == 0):
+        return (None)
+    result = list()
+    for i in my_list:
+        if (i % 2 == 0):
+            result.append(True)
+        else:
+            result.append(False)
+    return (result)
