f42fdf9dde81249fa258887a214a3752e3d5d155
Martins <oyedelenewton@gmail.com>
2023-12-02 23:57:00 -0800

    6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end="")
+            if (i[len(i) - 1] != j):
+                print(" ", end="")
+        print() 
