09ae4fdd68eccf4905d10a5d47cd76cfa760d155
Martins <oyedelenewton@gmail.com>
2023-12-03 00:25:55 -0800

    7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    new = list()
+    if (len(tuple_a) == 0):
+        a = 0
+        b = 0
+    elif (len(tuple_a) == 1):
+        a = tuple_a[0]
+        b = 0
+    elif (len(tuple_a) == 2):
+        a = tuple_a[0]
+        b = tuple_a[1]
+    if (len(tuple_b) == 0):
+        c = 0
+        d = 0
+    elif (len(tuple_b) == 1):
+        c = tuple_b[0]
+        d = 0
+    elif (len(tuple_b) == 2):
+        c = tuple_b[0]
+        d = tuple_b[1]
+    new = [a + c, b + d]
+    return (tuple(new))
