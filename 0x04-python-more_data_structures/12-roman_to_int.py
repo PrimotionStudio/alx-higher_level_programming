8ca0e403df6e7d9bc3a47f5d485a82d652d47142
Martins <oyedelenewton@gmail.com>
2023-12-07 01:38:26 -0800

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    return  (10)
+    if (not isinstance(roman_string, str) or roman_string == None):
+        return (0)
+    num = {
+          'I': 1,
+          'V': 5,
+          'X': 10,
+          'L': 50,
+          'C': 100,
+          'D': 500,
+          'M': 1000
+    }
+    su = 0
+    pr = 0
+    for i in reversed(roman_string):
+        val = num[i]
+        if (val < pr):
+            su -= val
+        else:
+            su += val
+        pr = val
+    return  (su)
