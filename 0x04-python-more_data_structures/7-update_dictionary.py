d8f315e4623b707f424ef4ba24d8bc5880ac45af
Martins <oyedelenewton@gmail.com>
2023-12-06 17:05:50 -0800

    8

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if (key not in a_dictionary.keys()):
-        a_dictionary.update({ key: value })
+        a_dictionary.update({key: value})
     else:
         a_dictionary[key] = value
     return (a_dictionary)
