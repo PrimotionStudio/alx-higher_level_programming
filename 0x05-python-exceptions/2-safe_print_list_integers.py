e11aa73c06a57e5892b5971305859ec281764094
Martins <oyedelenewton@gmail.com>
2023-12-18 15:41:53 -0800

    2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    count = 0
+    while (i < x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+            count += 1
+        except ValueError:
+            i += 1
+        except TypeError:
+            i += 1
+    print()
+    return (count)
