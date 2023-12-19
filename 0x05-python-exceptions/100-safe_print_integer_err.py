c7a17b9b5a56166769761c811fcfeaaac58ea6d9
Martins <oyedelenewton@gmail.com>
2023-12-18 16:04:27 -0800

    100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError) as e:
+        print("Exception: {}".format(e), file=stderr)
+        return (False)
