83d79654c0f550505ac892538b64c3f1b0be6e79
The Primotion Studio <oyedelenewton@gmail.com>
2023-12-20 01:30:47 +0100

    999

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,19 @@
+import math
+
+class MagicClass:
+    def __init__(self, radius):
+        """Initializes a MagicClass object with a given radius.
+        """
+        if not isinstance(radius, (int, float)):
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Calculates and returns the area of the circle.
+        """
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """Calculates and returns the circumference of the circle.
+        """
+        return 2 * math.pi * self.__radius
