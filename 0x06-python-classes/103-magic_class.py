116a226cd94bed31464ffd2dac88c981bb2d0bc9
The Primotion Studio <oyedelenewton@gmail.com>
2023-12-20 01:42:40 +0100

    999

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,24 +1,23 @@
-"""Initializes a MagicClass object with a given radius."""
+"""The File contais a class `MagicClass`."""
 
 
 import math
 
 
 class MagicClass:
-    """Initializes a MagicClass object with a given radius."""
+    """Creates a MagicClass object."""
+
     def __init__(self, radius):
-        """Initializes a MagicClass object with a given radius.
-        """
+        """Initializes a MagicClass object with a given radius."""
+        self.__radius = 0
         if not isinstance(radius, (int, float)):
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """Calculates and returns the area of the circle.
-        """
+        """Calculates and returns the area of the circle."""
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """Calculates and returns the circumference of the circle.
-        """
-        return 2 * math.pi * self.__radius
+        """Calculates and returns the circumference of the circle."""
+        return (2 * math.pi) * self.__radius
