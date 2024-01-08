dbb1a7673b3036c49faaec5b29c3e9226790a9a5
Martins <oyedelenewton@gmail.com>
2024-01-08 13:39:41 -0800

    0

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A mod used to check if an object is
+instance of a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    A function used to check ths
+    stat of an obj to a class
+    """
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that can is use
+for isinstance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    A func for isinstance of
+    Args:
+         obj
+         a_class
+    """
+
+    return isinstance(obj, a_class)
