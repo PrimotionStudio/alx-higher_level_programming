7ec269e7c96a815f333fa817e82a63a28afe004f
The Primotion Studio <oyedelenewton@gmail.com>
2024-01-11 04:24:07 +0100

    0

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,6 +6,11 @@ I just am ...
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """
+    This it the func
+    for so,ething
+    """
+
     with open(filename, "r") as f:
         lines = f.readlines()
     with open(filename, "w") as f:
