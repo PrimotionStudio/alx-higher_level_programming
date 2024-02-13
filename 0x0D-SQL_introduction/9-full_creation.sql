c8cb1fdbe9b9ed2d31a4b426614b19432f1be8fa
Martins <oyedelenewton@gmail.com>
2024-02-13 15:35:05 -0800

    0

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,14 @@
--- 4-first_table.sql
-CREATE TABLE IF NOT EXISTS `first_table` (
+-- 9-full_creation.sql
+CREATE TABLE IF NOT EXISTS `second_table` (
 	`id` INT,
-	`name` VARCHAR(256)
+	`name` VARCHAR(256),
+	`score` INT
 );
+INSERT INTO `second_table` (id, name, score)
+VALUES (1, "John", 10);
+INSERT INTO `second_table` (id, name, score)
+VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (id, name, score)
+VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (id, name, score)
+VALUES (4, "George", 8);
