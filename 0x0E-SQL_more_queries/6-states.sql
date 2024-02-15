dd06a73c8ec874e938702cd100445fa617e70fa8
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-15 22:42:15 +0100

    0

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- 6-states.sql
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (
+	`id` INT NOT AUTO_INCREMENT PRIMARY KEY,
+	`name` VARCHAR(256) NOT NULL
+);
