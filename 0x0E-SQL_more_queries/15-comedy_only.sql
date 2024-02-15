719ed574814e6b771189077a913568526588a8eb
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-16 00:51:10 +0100

    0

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -6,4 +6,4 @@ ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
 JOIN `tv_genres`
 ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
 WHERE `tv_genres`.`name` = 'Comedy'
-ORDER BY `tv_show`.`title`;
+ORDER BY `tv_shows`.`title`;
