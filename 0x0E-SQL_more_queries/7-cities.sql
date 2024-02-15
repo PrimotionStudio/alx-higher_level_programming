798d337408dbd1f50f0ae3237b59a1296cc985b4
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-15 22:53:24 +0100

    0

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS `cities` (
 	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` VARCHAR(256) NOT NULL,
 	`state_id` INT NOT NULL,
-	FOREIGN KEY `state_id` REFERENCES `state`(`id`)
+	FOREIGN KEY `state_id` REFERENCES `states`(`id`)
 );
