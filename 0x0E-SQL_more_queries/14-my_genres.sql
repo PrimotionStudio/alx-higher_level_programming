a2f0f1efe4cf6dbf81d3c1e6243611aa2591240b
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-16 00:37:09 +0100

    0

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- 14-my_genres.sql
-SELECT `tv_shows`.`name` AS `name`
-FROM `tv_shows`
-JOIN `tv_show_genres`, `tv_genres`
-ON `tv_genre` = 'Dexter'
+SELECT `tv_genres`.`name` AS `name`
+FROM `tv_genres`
+JOIN `tv_show_genres`, `tv_shows`
+ON `tv_shows`.`title` = 'Dexter'
 WHERE `tv_show_genres`.`genre_id` = `tv_genres`.`id` AND `tv_show_genres`.`show_id` = `tv_shows.id`
 ORDER BY `tv_genre`.`name` ASC;
