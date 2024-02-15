893a329748b156ab19e970f5a807209dcc3a143d
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-16 00:50:01 +0100

    0

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,7 +3,7 @@ SELECT `tv_shows`.`title`
 FROM `tv_shows`
 JOIN `tv_show_genres`
 ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
-JOIN `t_genres`
+JOIN `tv_genres`
 ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
 WHERE `tv_genres`.`name` = 'Comedy'
 ORDER BY `tv_show`.`title`;
