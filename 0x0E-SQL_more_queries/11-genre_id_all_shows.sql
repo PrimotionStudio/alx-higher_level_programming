72621b0f0c8359c9d97936d3de5ee96abd6e0b74
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-15 23:38:23 +0100

    0

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- 10-genre_id_by_show.sql
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+FROM `tv_shows`
+LEFT JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id` ASC;
