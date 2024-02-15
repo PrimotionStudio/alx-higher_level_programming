f8fd03fd03ae1ec33292aaaa9bf018123eaee481
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-15 23:51:42 +0100

    0

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- _12-no_genre.sql
 SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
 FROM `tv_shows`
-LEFT JOIN `tv_show_genres` ON `tv_shows`.`id` != `tv_show_genres`.`show_id`
+LEFT JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+WHERE `tv_show_genres`.`genres_id` IS NULL
 ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id` ASC;
