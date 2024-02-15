23db6c965b47c4d6e0b5d0d3901d4aa8024732e3
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-15 23:13:48 +0100

    0

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,5 @@
 -- 9-cities_by_state_join.sql
-SELECT `cities`.`id`, `cities`.`name`, `states`.`name` FROM `cities`, `states`
+SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
+FROM `cities`
+JOIN `states` ON `cities`.`state_id` = `states`.`id`
+ORDER BY `cities`.`id` ASC';
