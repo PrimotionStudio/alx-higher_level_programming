350cc76cd272c281f036ff78fd7d59ab649bc72a
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-15 23:07:03 +0100

    0

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- 8-cities_of_california_subquery.sql
-SELECT * FROM `cities` WHERE `state_id` IN (
+SELECT `id`, `name` FROM `cities` WHERE `state_id` IN (
 	SELECT `id`
 	FROM `states`
 	WHERE `name`='California')
