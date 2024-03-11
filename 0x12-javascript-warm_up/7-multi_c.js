7ba9376ec37444a98e04afc2313a4d12e1a8750e
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-11 21:53:43 +0100

    javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+let i = 0;
+const j = parseInt(argv[2]);
+if (!isNaN(j)) {
+  while (i < j) {
+    console.log('C is fun');
+    i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
