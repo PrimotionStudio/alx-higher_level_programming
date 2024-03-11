be3a57a61d5b7720c869f11d2b0d1298541ab8fc
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-11 20:43:06 +0100

    javasccript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 if (argv.length <= 2) {
-	console.log("No argument");
-} else if (argv.length == 3) {
-	console.log("Argument found");
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log("Arguments found");
+  console.log('Arguments found');
 }
