2225ab6e0d3468e57148483ef1578a11d2ea856b
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-11 22:08:14 +0100

    javascript

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+const i = parseInt(argv[2]);
+const j = parseInt(argv[3]);
+if (isNaN(i) || isNaN(j)) {
+  console.log('NaN');
+} else {
+  console.log(i + j);
+}
