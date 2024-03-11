9e5596046f010ccd43a43e9f86ea8c87e1f9d83a
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-11 22:03:58 +0100

    javascript

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+let i = 0;
+let row = '';
+const j = parseInt(argv[2]);
+if (!isNaN(j)) {
+  while (i < j) {
+    let k = 0;
+    while (k < j) {
+      row = row + 'X';
+      k++;
+    }
+    console.log(row);
+    row = '';
+    i++;
+  }
+} else {
+  console.log('Missing size');
+}
