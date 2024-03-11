515a78d2773075e3246558f7b5262055735782b3
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-11 21:14:13 +0100

    javascript

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const { argv } = require("node:process");
-console.log((argv[2] === undefined) ? "undefined" : argv[2], "is", (argv[3] === undefined) ? "undefined" : argv[3]);
+const { argv } = require('node:process');
+console.log((argv[2] === undefined) ? 'undefined' : argv[2], 'is', (argv[3] === undefined) ? 'undefined' : argv[3]);
