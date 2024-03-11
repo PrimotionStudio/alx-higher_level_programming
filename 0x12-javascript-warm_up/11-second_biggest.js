0212e2f13b683ac2dbed1abf6b0e2d523ecf994a
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-11 23:17:01 +0100

    javascript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  let i = 2;
+  let max = parseInt(argv[i]);
+  while (true) {
+    if (isNaN(parseInt(argv[i]))) {
+      break;
+    }
+    if (parseInt(argv[i]) > max) {
+      max = parseInt(argv[i]);
+    }
+    i++;
+  }
+  let j = 2;
+  let nextMax = parseInt(argv[j]);
+  while (j < i) {
+    if (parseInt(argv[j]) > nextMax && parseInt(argv[j]) !== max) {
+      nextMax = parseInt(argv[j]);
+    }
+    j++;
+  }
+  console.log(nextMax);
+}
