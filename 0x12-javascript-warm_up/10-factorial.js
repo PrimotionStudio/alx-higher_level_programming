4f7e4b2ccf41752a04104f727611d328730b9ac7
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-11 22:36:24 +0100

    javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+const i = parseInt(argv[2]);
+if (isNaN(i)) {
+  console.log(1);
+} else {
+  console.log(factorial(i));
+}
+function factorial (num) {
+  if (num === 1) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
