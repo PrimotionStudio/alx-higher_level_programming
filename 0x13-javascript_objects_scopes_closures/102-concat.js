fb38f9b6bee570c4f366fe6aef4a8d16898bd8ed
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-12 18:46:42 +0100

    javascript

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,14 @@
 const fs = require('fs');
 const [, , fileA, fileB, fileC] = process.argv;
 fs.readFile(fileA, 'utf8', (err, a) => {
+  if (err) {
+    return;
+  }
   fs.readFile(fileB, 'utf8', (err, b) => {
+    if (err) {
+      return;
+    }
     const concatenatedData = a + b;
     fs.writeFile(fileC, concatenatedData, 'utf8', () => {});
   });
 });
-
