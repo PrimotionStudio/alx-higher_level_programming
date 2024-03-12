73b97e65b1f24591a8fa2b66d861fde9548bf514
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-12 21:14:14 +0100

    javascript

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
 const dict = require('./101-data.js');
-let new = {};
-for (let key in dict) {
-  let n_key = dict[key];
-  for (let key in dict) {
-    if (dict[key] == n_key) {
-      new.n_key = key;
+let newDict = {};
+const keys = Object.keys(dict.dict);
+const vals = new Set(Object.values(dict.dict));
+vals.forEach(val => {
+    let numArray = [];
+  keys.forEach(key => {
+    if (dict.dict[key] === val) {
+      numArray.push(key);
     }
-  }
-}
+  });
+  newDict[val] = numArray;
+});
+
+console.log(newDict);
