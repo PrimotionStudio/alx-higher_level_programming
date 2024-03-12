a24ceee40dfc1fac2901bcae887a0c9a200c37af
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-12 18:03:07 +0100

    javascript

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Rectangle = require('./5-square');
-module.exports = class Square extends Rectangle{
-  constructor(size) {
+module.exports = class Square extends Rectangle {
+  constructor (size) {
     super(size, size);
   }
 
