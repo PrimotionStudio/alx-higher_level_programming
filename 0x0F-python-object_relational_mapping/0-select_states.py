8d41a4ca076477048342cf5d2db9614c45252276
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-14 19:02:58 +0100

    orm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+get all states
+"""
+from sys import argv
+import MySQLdb
+
+db = MySQLdb.connect(host='localhost:3306',
+                     user=argv[1], passwd=argv[2], db=argv[3])
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+rows = cur.fetchall()
+print(rows)
