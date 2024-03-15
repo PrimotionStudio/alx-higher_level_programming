b101e307386f5ede8446af243ca28400213b8f38
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-15 07:12:00 +0100

    orm

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost',
                          port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states\
+    query = "SELECT id, name FROM states\
              WHERE BINARY name=%s\
              ORDER BY id ASC"
     cur.execute(query, (argv[4],))
