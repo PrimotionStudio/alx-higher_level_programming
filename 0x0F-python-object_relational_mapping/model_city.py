418012212d39fcc72f3f57b18efc5e65002d16f5
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-15 17:19:12 +0100

    orm

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,7 +2,7 @@
 """
 cities in state
 """
-from sqlalchemy import Column, Integer, String, ForiegnKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -14,4 +14,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForiegnKey='states.id', nullable=False)
+    state_id = Column(Integer, ForeignKey='states.id', nullable=False)
