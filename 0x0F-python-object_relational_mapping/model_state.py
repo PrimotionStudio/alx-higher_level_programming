256127b37793f5abe295f2f25a245c551abdee00
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-15 14:28:14 +0100

    orm

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+first state model
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    first state model
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=false)
+    name = Column(String(128), nullable=false)
