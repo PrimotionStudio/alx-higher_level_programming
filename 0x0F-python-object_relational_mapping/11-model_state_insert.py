14e7fbb9d88bca8a551f30bd6db8bfd162758238
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-15 16:38:53 +0100

    orm

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name="Louisiana")
+    session.add(state)
+    session.commit()
+    print(state.id)
