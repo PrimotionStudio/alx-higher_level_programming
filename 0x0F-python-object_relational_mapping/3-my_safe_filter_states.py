00dde43a6f96d2647c16d9bb0da874a40c41ce05
Martins <oyedelenewton@gmail.com>
2024-03-14 22:35:45 -0700

    0

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+my filter states
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}' ORDER BY id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+my filter states
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities INNER JOIN states ON cities.state_id = states.id WHERE BINARY name='{}' ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
