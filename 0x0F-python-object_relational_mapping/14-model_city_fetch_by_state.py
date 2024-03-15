2636f019542e1096c270ee56aaa476cdbee664b9
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-15 17:14:50 +0100

    orm

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(City).join(State).filter(City.state_id == State.id).order_by(City.id)
+    print(query)
+    for city, state in query.all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+cities in state
+"""
+from sqlalchemy import Column, Integer, String, ForiegnKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class City(Base):
+    """
+    cities in state
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForiegnKey='states.id', nullable=False)
