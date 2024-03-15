9ca28e67c8032b3760190792cb32f6723a48660c
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-15 07:18:13 +0100

    orm

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+filter cities
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name='%s'\
+                ORDER BY id", argv[4])
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
