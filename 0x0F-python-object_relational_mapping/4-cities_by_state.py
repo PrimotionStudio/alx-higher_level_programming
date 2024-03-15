64c6f7f7ac273e3c9d3a1526d0f6ef4cd57dbb4c
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-15 07:09:23 +0100

    orm

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost',
                          port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities\
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
                 ORDER BY id")
     rows = cur.fetchall()
