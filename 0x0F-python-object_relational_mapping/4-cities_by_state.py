895c1da467792a6e28bf1fb4ae0bf24266439408
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-15 06:44:38 +0100

    orm

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost',
                          port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities INNER JOIN states ON cities.state_id = states.id WHERE BINARY name='{}' ORDER BY id")
+    cur.execute("SELECT * FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE BINARY name='{}'\
+                ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
