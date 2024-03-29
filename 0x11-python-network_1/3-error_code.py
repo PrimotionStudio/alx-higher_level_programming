b8508177cb608776215822e946bb2d426fca3261
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-29 02:13:15 +0100

    python network 2

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,6 +8,6 @@ from urllib import request
 if __name__ == "__main__":
     try:
         with request.urlopen(argv[1]) as response:
-            print(response.read())
+            print(response.read().decode("ascii"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
