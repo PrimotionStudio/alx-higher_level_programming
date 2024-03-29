b418738f0b59f24198f9d26c12531b822506fb8a
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-29 02:05:26 +0100

    python network 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,15 +2,15 @@
 """ython script that takes in a UR
 """
 from sys import argv
-from sys import request, parse
+from urlib import request, parse
 
 
 if __name__ == "__main__":
     url = argv[1]
     email = {'email' : argv[2] }
-    data = urllib.parse.urlencode(email)
+    data = parse.urlencode(email)
     data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
        the_page = response.read()
        print(the_page.decode('utf-8'))
