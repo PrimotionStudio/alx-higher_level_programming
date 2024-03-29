baa524413f00cfe7570e30fb47964a8a6605735f
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-29 02:01:46 +0100

    python nwtwork 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""ython script that takes in a UR
+"""
+from sys import argv
+from sys import request, parse
+
+
+url = argv[1]
+email = {'email' : argv[2] }
+
+data = urllib.parse.urlencode(email)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+   the_page = response.read()
+   print(the_page.decode('utf-8'))
