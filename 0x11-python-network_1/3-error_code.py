63be2f40f4da0c61385a40592a83f64350a3db7e
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-29 02:11:50 +0100

    python network 2

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""ython script that takes in a UR
+"""
+from sys import argv
+from urllib import request
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read())
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
