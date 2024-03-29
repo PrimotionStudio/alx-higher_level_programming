6db16fd2999737258430bcd7ce7a595ca8079bd1
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-29 01:53:09 +0100

    python nwtwork 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
 1-hbtn_header.py
+, sends a request to the URL and displays the
 """
 from sys import argv
 from urllib import request
 
 
-with request.urlopen(argv[1]) as response:
+req = request.Request(argv[1])
+with request.urlopen(req) as response:
     print(dict(response.headers).get('X-Request-Id'))
