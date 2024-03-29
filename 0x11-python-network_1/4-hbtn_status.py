79268386a9b72cff200456ea3e9b3a3d7c967aa7
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-29 02:22:19 +0100

    python network 2

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+ Python script that fetches
+"""
+import requests
+
+
+if __name__ == "__main__":
+    resp = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t-type: {}".format(type(resp.text)))
+    print("\t-content: {}".format(resp.text))
