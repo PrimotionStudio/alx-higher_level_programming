beeb34a2f938276eb2ccfd7bf92a0bf930e763f0
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-29 02:07:28 +0100

    python network 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,10 +7,10 @@ from urllib import request, parse
 
 if __name__ == "__main__":
     url = argv[1]
-    email = {'email' : argv[2] }
+    email = {'email': argv[2]}
     data = parse.urlencode(email)
     data = data.encode('ascii')
     req = request.Request(url, data)
     with request.urlopen(req) as response:
-       the_page = response.read()
-       print(the_page.decode('utf-8'))
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
