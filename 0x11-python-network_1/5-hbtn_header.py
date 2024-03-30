2a41a704e71aa0910ebc132b66c2f9cf0355882b
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-30 09:27:14 +0100

    python network

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    print(str(req.__dict__['headers']['X-Request-Id']))
