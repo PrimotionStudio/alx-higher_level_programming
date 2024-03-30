e80fdcd74f9fffbba6393f632a31e7cbce346427
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-30 09:46:44 +0100

    python network

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+ Python script that takes in a URL
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
