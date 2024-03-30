0c69f582669723ee7ef3339972c9e6a52a86d507
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-30 11:08:24 +0100

    python network

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     h = {"Accept": "application/vnd.github+json",
          "Authorization": "Bearer {}".format(argv[2]),
          "X-GitHub-Api-Version": "2022-11-28"}
-    res = requests.get("https://api.github.com/user", headers=h, auth=(argv[1], argv[2]))
+    res = requests.get("https://api.github.com/user",
+                       headers=h, auth=(argv[1], argv[2]))
     if "id" in res.json().keys():
         print(res.json()["id"])
     else:
