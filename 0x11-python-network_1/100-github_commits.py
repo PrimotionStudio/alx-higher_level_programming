6d93039eee26344acdefbbc1018358c81d065022
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-30 11:37:33 +0100

    python network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+github something
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    h = {"Accept": "application/vnd.github+json",
+         "X-GitHub-Api-Version": "2022-11-28"}
+    res = requests.get("https://api.github.com/repos/{}/{}/commits"
+                       .format(argv[1], argv[2]), headers=h)
+    objs = sorted(res.json(), reverse=True,
+                  key=lambda x: x['commit']['author']['date'])[:10]
+    for i in objs:
+        print("{}: {}".format(i['sha'], i['commit']['author']['name']))
