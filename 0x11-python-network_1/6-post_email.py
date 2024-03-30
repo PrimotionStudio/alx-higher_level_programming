4544342dfc4788d3ab2db88f21a937ba430eb7bc
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-30 09:41:01 +0100

    python network

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+ Python script that takes in a URL
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    payload = {'email': argv[2]}
+    res = requests.post(argv[1], data=payload)
+    print(res.text)
