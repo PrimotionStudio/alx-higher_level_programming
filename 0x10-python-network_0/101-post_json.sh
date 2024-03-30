1f306fcbc8ef48f1b9624be6be62d4db1de08021
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-30 08:50:42 +0100

    python network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 101 post json sh
-curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d $2 $1
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d '$2' $1
