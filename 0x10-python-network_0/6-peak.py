e092a03d4939300cfad51e47a888fedc4c09a6d7
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-30 08:32:57 +0100

    python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 def find_peak(list_of_integers):
-    i = list_of_integers
-    if i == []:
+    x = list_of_integers
+    i = 1
+    if x == []:
         return (None)
-    return (max(i))
+    peak = x[i]
+    if len(x) < 3:
+        return (max(x))
+    while i < len(x) - 1:
+        try:
+            if x[i] > x[i + 1] and x[i] > x[i - 1]:
+                peak = x[i]
+            i += 1
+        except IndexError:
+            return peak
+    return peak
+
