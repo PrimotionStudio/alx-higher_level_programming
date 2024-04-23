816e84495e153679819c50facc10f42d8afa03d9
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-23 15:46:12 +0100

    javascript

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', (err, data) => {
+  if (err) console.error(err);
+  else console.log(data);
+});
