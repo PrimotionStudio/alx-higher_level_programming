12a49011b7a25a3670bcc4d58ec62e4c14eb87b1
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-23 15:47:51 +0100

    javascript

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 
 const url = process.argv[2];
 request.get(url, (err, data) => {
-	if (err) console.error(err);
-	else console.log(`code: ${data}`);
+  if (err) console.error(err);
+  else console.log(`code: ${data}`);
 });
