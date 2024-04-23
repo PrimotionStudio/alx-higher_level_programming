ae759beff3683bb7d67f2b06534f641612074330
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-23 16:42:53 +0100

    javascript

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = process.argv[2];
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request.get(url, (err, data) => {
   if (err) console.error(err);
-  else console.log(`code: ${data.statusCode}`);
+  else console.log(JSON.parse(data.body).title);
 });
