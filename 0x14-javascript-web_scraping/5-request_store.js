400101b211605263d3835b60330ecd6657b7265b
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-24 02:53:42 +0100

    javascript

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,10 +4,9 @@ const fs = require('fs');
 
 const url = process.argv[2];
 const file = process.argv[3];
-request.get(url, (err, data) => {
+request.get(url, (err, data, html) => {
   if (err) console.error(err);
   else {
-    const html = JSON.parse(data.body);
     fs.writeFile(file, html, {encoding: 'utf-8'}, (err) => {
     	console.error(err);
     });
