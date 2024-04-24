ae356014980dc71a098f4c286ff755eea2e023fd
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-24 02:15:09 +0100

    javascript

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 
-const id = 18;
-const url = `https://swapi-api.alx-tools.com/api/people/${id}`;
+const id = `https://swapi-api.alx-tools.com/api/people/18/`;
+// const url = `https://swapi-api.alx-tools.com/api/people/${id}`;
+const url = `${process.argv[2]}`;
 request.get(url, (err, data) => {
   if (err) console.error(err);
-  else console.log(JSON.parse(data.body).films.length);
+  else {
+    const films = JSON.parse(data.body).results;
+    let count = 0;
+    films.forEach((film) => {
+      if (film.characters.includes(id)) count++;
+    });
+    console.log(count);
+  }
 });
